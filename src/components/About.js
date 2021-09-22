9345d7428fcefd1eef80e32e83cbc6605746ae8d
Steven <shyfay0@gmail.com>
2021-09-22 09:27:51 -0700

    changed user.bio to props.bio and solved all the problems, it's now finished

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import React from "react";
 import user from "../data/user";
 import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {user.bio ? <p>{user.bio}</p> : null }
+      {props.bio ? <p>{props.bio}</p> : null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links />
     </div>
